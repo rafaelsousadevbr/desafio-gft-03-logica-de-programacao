5321944dcc57d96885059cb23c2d3db54ffcda54
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-10-06 16:44:38 -0300

    criado os objetos para testar o tipo de ataque de cada herói

--- index.js
+++ index.js
@@ -16,10 +16,23 @@ class hero {
         } else if (this.type === 'monge') {
             console.log(`o ${this.type} atacou usando artes marciais`);
 
-        } else {
+        } else if (this.type === 'ninja') {
             console.log(`o ${this.type} atacou usando shuriken`);
 
+        } else{
+            console.log(`tipo de guerreiro não especificado`)
         }
-
     }
 }
+
+const hero01 =  new hero('Veridiano', 37, 'mago');
+hero01.attack()
+
+const hero02 =  new hero('Dante', 28, 'guerreiro');
+hero02.attack()
+
+const hero03 =  new hero('Jackie Chan', 43, 'monge');
+hero03.attack()
+
+const hero04 =  new hero('Roronoa Zoro', 23, 'ninja');
+hero04.attack()
