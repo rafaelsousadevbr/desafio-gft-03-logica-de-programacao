cc0ff7ca4e987587bf117d6d703b25f1ee5a8dc5
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-10-06 16:25:19 -0300

    criado o método attack(ataque) da classe hero

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-class hero{
+class hero {
 
-    constructor(name, age, type){
+    constructor(name, age, type) {
         this.name = name;
         this.age = age;
         this.type = type;
+    }
+
+    attack() {
+        if (this.type === 'mago') {
+            console.log(`o ${this.type} atacou usando magia`);
+
+        } else if (this.type === 'guerreiro') {
+            console.log(`o ${this.type} atacou usando espada`);
+
+        } else if (this.type === 'monge') {
+            console.log(`o ${this.type} atacou usando artes marciais`);
+
+        } else {
+            console.log(`o ${this.type} atacou usando shuriken`);
+
+        }
+
     }
 }
